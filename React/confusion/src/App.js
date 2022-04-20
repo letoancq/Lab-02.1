5918d362f1f82bd32eacfc02a580a9001a1eef78
ToanFX15903 <vtv5hdcq@gmail.com>
2022-04-20 19:43:08 +0700

    Configuring react

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,16 +1,19 @@
-import { Component } from 'react';
-import {Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+class App extends Component {
 
-class App extends Component{
-  render(){
+  render() {
     return (
-      <div className='App'>
-        <Navbar dark color="primary">
+      <div className="App">
+        <Navbar dark color={"primary"}>
           <div className='container'>
-            <NavbarBrand href='/'Ristorante Con Fusion></NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
       </div>
     );
   }
 }
+
+export default App;
